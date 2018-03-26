62e5354a4e1896ade5bc6aa32f1b71ec83a6eb00
dan kiss and Emily Watkins <emily@>
2018-03-26 12:02:52 -0700

    setup basic angular project

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,24 @@
 import { Component } from '@angular/core';
+import { Task } from './models/task.model'
 
 @Component({
-  selector: 'app-root',
+  selector: 'crazy-app-selector2',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  currentFocus: string = 'Angular Homework';
+  currentTime = new Date();
+  month: number = this.currentTime.getMonth() + 1;
+  day: number = this.currentTime.getDate();
+  year: number = this.currentTime.getFullYear();
+  // firstTask: Task = new Task("Finish weekend Angular homework for Epicodus Course");
+  tasks: Task[] = [
+    new Task('Finish weekend Angular homework for Epicodus course'),
+    new Task('Begin brainstorming possible JS group projects'),
+    new Task('Add README file to last few Angular repos on Github')
+  ];
+
 }
+
+// console.log(firstTask.done);
